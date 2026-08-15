8c73adfe14a8b1103683c8f8276bafc5a352c350
bradengrooms <bgroo2004@gmail.com>
2026-08-14 22:16:11 -0500

    fix main.jsx import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./vendor/normalize.css";
-import "./index.css";
+import "./blocks/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
